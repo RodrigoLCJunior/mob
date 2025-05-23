0e13120e90dcf71362ef423f4694255e8bbf6883
FilipeDSADS <filipeaugustodsds@gmail.com>
2025-05-23 12:40:45 -0300

    a
    a

--- backend/jogo/src/main/java/com/example/jogo/controller/DungeonController.java
+++ backend/jogo/src/main/java/com/example/jogo/controller/DungeonController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/dungeons")
+@RequestMapping("/api/dungeon")
 public class DungeonController {
 
     @Autowired
